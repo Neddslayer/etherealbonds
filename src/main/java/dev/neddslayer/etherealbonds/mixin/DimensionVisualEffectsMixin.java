099e20f374c0091f8e7d492a5f078d35aa4b2b9b
Neddslayer <neddslayer32@gmail.com>
2023-01-31 11:10:57 -0600

    upgrade visual effect mixin to not overwrite class

--- src/main/java/dev/neddslayer/etherealbonds/mixin/DimensionVisualEffectsMixin.java
+++ src/main/java/dev/neddslayer/etherealbonds/mixin/DimensionVisualEffectsMixin.java
@@ -12,12 +12,19 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DimensionVisualEffects.class)
 public class DimensionVisualEffectsMixin {
 
 
-    private static final Object2ObjectMap<Identifier, DimensionVisualEffects> BY_IDENTIFIER =
+    @Shadow
+    @Final
+    private static Object2ObjectMap<Identifier, DimensionVisualEffects> BY_IDENTIFIER;
+    private static final Object2ObjectMap<Identifier, DimensionVisualEffects> BY_IDENTIFIER_CUSTOM =
         (Object2ObjectMap<Identifier, DimensionVisualEffects>) Util.make(new Object2ObjectArrayMap(), (map) -> {
         DimensionVisualEffects.Overworld overworld = new DimensionVisualEffects.Overworld();
         map.defaultReturnValue(overworld);
@@ -27,13 +34,18 @@ public class DimensionVisualEffectsMixin {
         map.put(EtherealPlaneVisualEffect.PLANE_ID, new EtherealPlaneVisualEffect());
     });
 
-    /**
-     * @author me
-     * @reason because javadocs needed it
-     */
-    @Overwrite
-    public static DimensionVisualEffects byDimensionType(DimensionType dimensionType) {
-        return (DimensionVisualEffects)BY_IDENTIFIER.get(dimensionType.effectsLocation());
+    //@Overwrite
+    //public static DimensionVisualEffects byDimensionType(DimensionType dimensionType) {
+    //    return (DimensionVisualEffects)BY_IDENTIFIER.get(dimensionType.effectsLocation());
+    //}
+
+    //byDimensionType(Lnet/minecraft/world/DimensionType)Lnet/minecraft/client/render/DimensionVisualEffects
+
+    @Inject(at = @At("RETURN"), method = "byDimensionType", cancellable = true)
+    private static void overrideType(DimensionType dimensionType, CallbackInfoReturnable<DimensionVisualEffects> ci) {
+        if (!BY_IDENTIFIER.containsKey(dimensionType.effectsLocation())) {
+            ci.setReturnValue(BY_IDENTIFIER_CUSTOM.get(dimensionType.effectsLocation()));
+        }
     }
 
 }
